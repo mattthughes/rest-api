4ed9cfb34581db98bfba0156a42b28ee73f296b2
Matthew Hughes <matt.h17@yahoo.com>
2024-06-05 17:08:32 +0000

    Added comment generic view

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
+    path('', include('comments.urls')),
+
 
 ]
