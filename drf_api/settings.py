5224618bcc40a4013c55f8d03ceab6088187b998
Matthew Hughes <matt.h17@yahoo.com>
2024-06-05 19:59:45 +0000

    changed£

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-mattthughes-restapi-6hg9aub1n2z.ws-eu114.gitpod.io', 'restt-framework-api.herokuapp.com']
+ALLOWED_HOSTS = ['8000-mattthughes-restapi-6hg9aub1n2z.ws-eu114.gitpod.io', '.herokuapp.com']
 
  
 # Application definition
