7f298b57c35c9a3c899eb00c408f97a771451172
Matthew Hughes <matt.h17@yahoo.com>
2024-06-05 17:32:43 +0000

    updated

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from posts.models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,10 +8,23 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-
+    like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
+    
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner = user, post = obj
+            ).first()
+            return like.id if like else None
+        return None
+
     
     def validate_image(self, value):
         if value.size > 1024 * 1024 *2:
@@ -33,5 +47,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image'
+            'title', 'content', 'image', 'like_id'
         ]
