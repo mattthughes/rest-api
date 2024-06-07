9297a874f1970bf0a4193c011b2237fbd41d08d3
Matthew Hughes <matt.h17@yahoo.com>
2024-06-07 19:40:52 +0000

    adjusted settings cors

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -66,10 +66,7 @@ ALLOWED_HOSTS = [
    'localhost',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
+CORS_ALLOWED_ORIGINS = ('https://moments-mh-c5d7075da488.herokuapp.com/')
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
