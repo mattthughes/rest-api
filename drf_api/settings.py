650ee9eaec567a9dd8c1acb5c6db958cd00d582a
Matthew Hughes <matt.h17@yahoo.com>
2024-06-08 09:51:01 +0000

    updated settings.py file

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -109,7 +109,6 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 MIDDLEWARE = [
-    'allauth.account.middleware.AccountMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
